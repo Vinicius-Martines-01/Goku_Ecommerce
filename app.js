c1e27409bd3052447461ef1393ba35b0c849b9d7
Vinicius-Martines-01 <vinicius.alcantarilla@fatec.sp.gov.br>
2024-09-17 21:35:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function dados(){
         { id: 1, login: "gabriel", password: "1234@", email: "gabriel@gmail.com"},//[0]
         { id: 2, login: "amanda", password: "12345@", email: "amanda@gmail.com"},//[1]
         { id: 3, login: "ladygaga", password: "123456@", email: "ladygaga@gmail.com"},//[2]
-        { id: 3, login: "snoopy", password: "1950", email: "snoopy@gmail.com"}
+        { id: 4, login: "snoopy", password: "1950", email: "snoopy@gmail.com"}
      ]
      return ds
 }
