95be7234c2b2185f976b9202cf32bf0fc7a02fd6
Vinicius-Martines-01 <vinicius.alcantarilla@fatec.sp.gov.br>
2024-11-18 12:15:15 -0300

    v0.2

--- scripts/app.js
+++ scripts/app.js
@@ -33,8 +33,8 @@ function login(event){
 function vec_produtos(){
     const os_produtos = [ 
         { id: 0, nome: "Camiseta Goku Masculina", img: 'img/camisetagoku.jpg', preco: '59,99', val: 59.99},
-        { id: 1, nome: "carrinho.html", img: 'img/canecagoku2.webp', preco: ''},
-        { id: 2, nome: "gabriel", img: '', preco: ''},
+        { id: 1, nome: "Boneco Goku Super Saiajin", img: 'img/bonecodogoku.jpg', preco: '159,99', val: 159.99},
+        { id: 2, nome: "Caneca Goku", img: 'img/canecagoku2.webp', preco: '39,99', val:39.99},
         ]
     let prod = JSON.stringify(os_produtos);
     sessionStorage.setItem("produtos", prod);   
@@ -64,13 +64,13 @@ function add_produtos_to_main(){
     let produto_cards = ''
 
     for(let i = 0; i < tds_produtos.length; i++){
-        produto_cards += `<div class='card-produto'>
+        produto_cards += `<div value='${tds_produtos[i].id}' class='card-produto'>
         <img src=${tds_produtos[i].img} class="card-img-top">
         <h5>${tds_produtos[i].nome}</h5>
         <p> R$${tds_produtos[i].preco}</p>
         <div class='buttom-pos'>
             <p class='btns_texto'>Adicionar ao Carrinho</p>
-            <buttom value='${tds_produtos[i].id}' class='btn-comprar' type='button' onclick="add_ao_carinho(this,${i})"></buttom>
+            <buttom  class='btn-comprar' type='button' onclick="add_ao_carinho(this,${i})"></buttom>
         </div>
        
         </div>`;
@@ -95,7 +95,9 @@ function add_ao_carinho(own, i){
     if (prod_list.includes(tds_produtos[i].id)){
         console.log('já tem')
     }else {
-        let compra = {nome:tds_produtos[i].nome, valor:tds_produtos[i].val, pos:carrinho_j.length, id:tds_produtos[i].id}
+        let compra = {nome:tds_produtos[i].nome, img:tds_produtos[i].img, qtd:1, 
+            preco:tds_produtos[i].preco,  valor:tds_produtos[i].val, 
+            pos:carrinho_j.length, id:tds_produtos[i].id}
         carrinho_j.push(compra)
         carrinho_j[0].total += tds_produtos[i].val
         carrinho_j[0].qtd += 1
@@ -116,7 +118,21 @@ function load_carrinho_qtd(){
     const carrinho_j = JSON.parse(sessionStorage.getItem("carrinho"));
 
     const carrinho_qtd = document.getElementById('carrinho_qtd');
-    carrinho_qtd.innerHTML = carrinho_j[0].qtd;
+    const carrinho_box = document.getElementById('carrinho-box');
+    carrinho_j[0].qtd = carrinho_j.length - 1
+    if (carrinho_qtd.innerHTML != carrinho_j[0].qtd){
+        
+        carrinho_qtd.style.animation = 'none';
+        carrinho_qtd.offsetHeight;
+        carrinho_qtd.style.animation = null;
+
+        carrinho_box.style.animation = 'none';
+        carrinho_box.offsetHeight;
+        carrinho_box.style.animation = null;
+
+        carrinho_qtd.innerHTML = carrinho_j[0].qtd;
+    } 
+
 
 }
 
@@ -144,4 +160,100 @@ function change_buttons(){
         }
     });
 
+}
+
+function load_compras(){
+    const carrinho_j = JSON.parse(sessionStorage.getItem("carrinho"))
+    const items_carrinho = document.getElementById('items-do-carrinho')
+    
+    let produto_cards = ''
+    if (carrinho_j !== null){
+        for(let i = 1; i < carrinho_j.length; i++){
+            produto_cards += `<div value="${carrinho_j[i].id}" class='card-carrinho'>
+            <img src=${carrinho_j[i].img} class="card-img-top">
+            <div class="card-carrinho-div">
+                <h3>${carrinho_j[i].nome}</h3>
+                <p> R$${carrinho_j[i].preco}</p>
+            </div>
+            <nav class="card-carrinho-btns">
+                <button id="mais" onclick="carrinho_nav(this, 1)"></button>
+                <button id="menos" onclick="carrinho_nav(this, -1)"></button>
+            </nav>
+            <div class="card-carrinho-qtd">
+                <p>${carrinho_j[i].qtd}</p>
+                
+            </div>
+            <button id="remover" onclick="remove_do_carrinho(this)"></button>
+            </div>`;
+        }
+    }
+    items_carrinho.innerHTML = ''
+    items_carrinho.innerHTML += produto_cards
+}
+
+function carrinho_nav(own, v){
+    const carrinho_j = JSON.parse(sessionStorage.getItem("carrinho"))
+    
+    const id = own.parentNode.parentNode.getAttribute('value');
+    const qtd = own.parentNode.nextElementSibling.children[0];
+    console.log(carrinho_j)
+    console.log(id)
+    console.log(carrinho_j[id])
+
+
+    for(let i = 0;i < carrinho_j.length; i++){
+        if(id == carrinho_j[i].id){
+            console.log('pos '+id)
+            if (v < 0 && carrinho_j[i].qtd > 1){
+                carrinho_j[i].qtd += v 
+            } else if (v > 0) {
+                carrinho_j[i].qtd += v
+            }
+            
+            qtd.innerHTML = carrinho_j[i].qtd
+            
+            let carrinho = JSON.stringify(carrinho_j);
+            sessionStorage.setItem("carrinho", carrinho);  
+
+            calcular_total()
+        }
+}
+
+}
+
+function calcular_total(){
+    const carrinho_j = JSON.parse(sessionStorage.getItem("carrinho"))
+    const mostrar_total = document.getElementById('mostrar-total')
+
+    const valor = []
+    let total = 0
+
+    for(let i = 1;i < carrinho_j.length; i++){
+        valor.push(carrinho_j[i].valor * carrinho_j[i].qtd)
+        total += carrinho_j[i].valor * carrinho_j[i].qtd
+    }
+    carrinho_j[0].total = total
+
+    let carrinho = JSON.stringify(carrinho_j);
+    sessionStorage.setItem("carrinho", carrinho);  
+
+    console.log(carrinho)
+    mostrar_total.innerHTML = `Total: R$ ${carrinho_j[0].total.toFixed(2)}`
+}
+
+function remove_do_carrinho(own){
+    const carrinho_j = JSON.parse(sessionStorage.getItem("carrinho"))
+    
+    const id = own.parentNode.getAttribute('value');
+    console.log(id)
+    for(let i = 0;i < carrinho_j.length; i++){
+        if(id == carrinho_j[i].id){
+            carrinho_j.splice(i, 1)
+            //console.log(id, carrinho_j[i].id)
+            let carrinho = JSON.stringify(carrinho_j);
+            sessionStorage.setItem("carrinho", carrinho);  
+            load_compras()
+            calcular_total()
+        }
+    }
 }
